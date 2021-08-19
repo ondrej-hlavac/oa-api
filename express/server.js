95cd96a47e8efd2d1990925459e2a9883aaade21
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-08-19 04:00:24 +0200

    uppdate cors

--- express/server.js
+++ express/server.js
@@ -1,12 +1,30 @@
 'use strict';
 require('encoding');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const serverless = require('serverless-http');
 
 // init server app
 const app = express();
 app.use(express.json());
+// app.use(cors({origin: false}))
+
+const allowedOrigins = ['http://localhost:3000',
+  'https://festive-carson-cbfab3.netlify.app/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // read .env
 require('dotenv').config();
