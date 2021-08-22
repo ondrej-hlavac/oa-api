fd28234a42687e93a4f3cdf1bce51093ef126f0e
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2021-08-22 23:10:46 +0200

    cors update

--- express/server.js
+++ express/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // app.use(cors({origin: false}))
 
 const allowedOrigins = ['http://localhost:3000',
-  'https://osobniarcheologie.netlify.app/'];
+  'https://osobniarcheologie.netlify.app'];
 app.use(cors({
   origin: function(origin, callback){
     if(!origin) return callback(null, true);
